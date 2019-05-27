fa5fd3d490b51bfd070fab376370fe17d40e1e32
Christian Krohmer <32412683+badener95@users.noreply.github.com>
2019-05-27 16:35:02 +0200

    Initial push

--- app/src/main/java/de/badener/links/MainActivity.java
+++ app/src/main/java/de/badener/links/MainActivity.java
@@ -1,29 +1,43 @@
 package de.badener.links;
 
 import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Icon;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
 
-import com.anthonycr.progress.AnimatedProgressBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RelativeLayout topBarContainer;
+    private AppCompatTextView textViewURL;
+    private ProgressBar progressBar;
     private WebView webView;
-    private AnimatedProgressBar progressBar;
+    private BottomNavigationView bottomNavigation;
     private FrameLayout fullScreen;
 
-    private Boolean isFullScreen = false;
+    private static final String startPage = "https://www.google.com/";
+    private Icon launcherIcon;
+    private boolean isFullScreen = false;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -31,9 +45,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = findViewById(R.id.WebView);
-        progressBar = findViewById(R.id.ProgressBar);
-        fullScreen = findViewById(R.id.FullScreen);
+        topBarContainer = findViewById(R.id.topBarContainer);
+        textViewURL = findViewById(R.id.textViewURL);
+        progressBar = findViewById(R.id.progressBar);
+        webView = findViewById(R.id.webView);
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+        fullScreen = findViewById(R.id.fullScreenContainer);
 
         // WebView options
         webView.getSettings().setJavaScriptEnabled(true);
@@ -43,16 +60,69 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setBuiltInZoomControls(true);
         webView.getSettings().setDisplayZoomControls(false);
 
+        // Handle the bottom navigation bar
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_close:
+                        finish();
+                        break;
+                    case R.id.action_reload:
+                        webView.reload();
+                        break;
+                    case R.id.action_home:
+                        if (!webView.getUrl().equals(startPage)) {
+                            webView.loadUrl(startPage);
+                        }
+                        break;
+                    case R.id.action_pin:
+                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                            Intent addToHome = new Intent(MainActivity.this, MainActivity.class);
+                            addToHome.setData(Uri.parse(webView.getUrl()));
+                            addToHome.setAction(Intent.ACTION_MAIN);
+                            getLauncherIcon();
+                            String title = webView.getTitle();
+                            ShortcutInfo shortcutInfo = new ShortcutInfo.Builder(MainActivity.this, title)
+                                    .setShortLabel(title)
+                                    .setIcon(launcherIcon)
+                                    .setIntent(addToHome)
+                                    .build();
+                            getSystemService(ShortcutManager.class).requestPinShortcut(shortcutInfo, null);
+                        } else {
+                            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                            intent.setData(Uri.parse(webView.getUrl()));
+                            intent.setAction(Intent.ACTION_MAIN);
+                            getLauncherIcon();
+                            Intent addIntent = new Intent();
+                            addIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, webView.getTitle());
+                            addIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON, launcherIcon);
+                            addIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, intent);
+                            addIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+                            sendBroadcast(addIntent);
+                        }
+                        break;
+                    case R.id.action_share:
+                        Intent share = new Intent(Intent.ACTION_SEND);
+                        share.setType("text/plain");
+                        share.putExtra(Intent.EXTRA_TEXT, webView.getUrl());
+                        startActivity(Intent.createChooser(share, getString(R.string.share_url)));
+                        break;
+                }
+                return false;
+            }
+        });
+
         // Handle intents
         Intent intent = getIntent();
-        Uri data = intent.getData();
+        Uri uri = intent.getData();
         String url;
-        if (data != null) {
+        if (uri != null) {
             // App was opened from another app
-            url = data.toString();
+            url = uri.toString();
         } else {
             // App was opened from launcher
-            url = "https://www.google.com";
+            url = startPage;
         }
 
         // Load either the start page (Google in this case) or the URL provided by an intent
@@ -60,11 +130,23 @@ public class MainActivity extends AppCompatActivity {
 
         webView.setWebChromeClient(new WebChromeClient() {
 
+            // Update the progress bar
+            @Override
+            public void onProgressChanged(WebView view, int progress) {
+                progressBar.setProgress(progress);
+                if (progress < 100) {
+                    progressBar.setVisibility(View.VISIBLE);
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+
             // Enter fullscreen
             @Override
             public void onShowCustomView(View view, WebChromeClient.CustomViewCallback callback) {
                 super.onShowCustomView(view, callback);
-                webView.setVisibility(View.GONE);
+                topBarContainer.setVisibility(View.GONE);
+                bottomNavigation.setVisibility(View.GONE);
                 fullScreen.setVisibility(View.VISIBLE);
                 fullScreen.addView(view);
                 enableFullScreen();
@@ -76,46 +158,47 @@ public class MainActivity extends AppCompatActivity {
             public void onHideCustomView() {
                 super.onHideCustomView();
                 fullScreen.setVisibility(View.GONE);
-                webView.setVisibility(View.VISIBLE);
+                topBarContainer.setVisibility(View.VISIBLE);
+                bottomNavigation.setVisibility(View.VISIBLE);
                 setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER);
                 getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
                 isFullScreen = false;
             }
-
-            // Update the progress bar
-            @Override
-            public void onProgressChanged(WebView view, int progress) {
-                progressBar.setProgress(progress);
-            }
         });
 
+        // Display URL in the top bar
         webView.setWebViewClient(new WebViewClient() {
-
-            // Show the progress bar on page started loading
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                progressBar.setVisibility(View.VISIBLE);
+                textViewURL.setText(webView.getUrl());
             }
 
-            // Hide the progress bar on page finished loading
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                progressBar.setVisibility(View.GONE);
-            }
-
-            // Handle intents
+            // Handle some external links
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
                 String url = request.getUrl().toString();
-                if (url.startsWith("mailto:") || url.startsWith("tel:") || url.startsWith("sms:") || url.startsWith("whatsapp:")) {
-                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                    startActivity(intent);
+                if (url.startsWith("mailto:") || url.startsWith("tel:") || url.startsWith("sms:")
+                        || url.startsWith("whatsapp:") || url.contains("play.google.com")) {
+                    try {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        startActivity(intent);
+                    } catch (ActivityNotFoundException ignored) {
+                    }
                     return true;
                 } else {
                     return false;
                 }
             }
         });
+    }
+
+    // Handle new intents
+    @Override
+    public void onNewIntent(Intent intent) {
+        Uri uri = intent.getData();
+        if (uri != null && !uri.toString().equals(webView.getUrl())) {
+            webView.loadUrl(uri.toString());
+        }
     }
 
     // Restore fullscreen after losing and gaining focus
@@ -138,6 +221,15 @@ public class MainActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    // Get favicon if available for launcher shortcuts
+    private void getLauncherIcon() {
+        if (webView.getFavicon() != null) {
+            launcherIcon = Icon.createWithBitmap(webView.getFavicon());
+        } else {
+            launcherIcon = Icon.createWithResource(MainActivity.this, R.mipmap.ic_launcher);
+        }
     }
 
     // Prevent the back-button from closing the app
