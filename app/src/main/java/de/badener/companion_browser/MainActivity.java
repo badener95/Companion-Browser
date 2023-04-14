56c09f746939ca0e27d2a575f8b48fb42708e93a
Christian Krohmer <32412683+badener95@users.noreply.github.com>
2023-04-14 16:13:02 +0200

    Add recents url sharing on Pixel devices

--- app/src/main/java/de/badener/companion_browser/MainActivity.java
+++ app/src/main/java/de/badener/companion_browser/MainActivity.java
@@ -2,6 +2,7 @@ package de.badener.companion_browser;
 
 import android.annotation.SuppressLint;
 import android.app.DownloadManager;
+import android.app.assist.AssistContent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -487,6 +488,13 @@ public class MainActivity extends AppCompatActivity {
     private void enableFullScreen() {
         View decorView = getWindow().getDecorView();
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    // Enable recents URL sharing (Google Pixel devices only)
+    @Override
+    public void onProvideAssistContent(AssistContent outContent) {
+        super.onProvideAssistContent(outContent);
+        outContent.setWebUri(Uri.parse(webView.getUrl()));
     }
 
     // Prevent the back button from closing the app
